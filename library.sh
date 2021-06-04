ad6030ddd03192b4a5880d3b5e6786f76b9d53c2
Matthias Bock <mail@matthiasbock.net>
2021-06-04 13:48:00 +0200

    Return to previous path before proceeding with script execution

--- library.sh
+++ library.sh
@@ -6,10 +6,14 @@
 
 export container_cli="$(which podman)"
 
+save_pwd=$(realpath $(pwd))
 cd $(realpath $(dirname "${BASH_SOURCE[0]}"))
+
 source networking.sh
 source images.sh
 source volumes.sh
 source containers.sh
 source utils.sh
 source debian.sh
+
+cd "${save_pwd}"
