089ef3859c42cd0186a5301e4ed89931534e0431
Matthias Bock <mail@matthiasbock.net>
2021-10-13 17:31:41 +0200

    Draft: Delete container ancestory

--- containers.sh
+++ containers.sh
@@ -109,6 +109,30 @@ function container_stop()
 }
 
 
+#
+# This function flattens the given container's history,
+# i.e. makes it look as if it was created from scratch.
+# This is accomplished by exporting the container's content
+# and re-importing it as a new container replacing the original one.
+#
+function container_flatten()
+{
+  container_name="$1"
+  if !container_exists $container_name; then
+    echo "Error: Failed to flatten container $container_name: Container not found."
+    return 1
+  fi
+
+  if container_is_running $container_name; then
+    container_stop $container_name
+  fi
+
+  # TODO: Export container as archive
+  # TODO: Delete container
+  # TODO: Re-import container (without history) from archive
+}
+
+
 function container_commit()
 {
 	local container_name="$1"
