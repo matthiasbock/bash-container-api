1b110d667d708137ccd7756d7d5976800e472f4a
Matthias Bock <mail@matthiasbock.net>
2023-01-27 16:28:17 +0100

    Moving function to commit a container to images.sh

--- containers.sh
+++ containers.sh
@@ -133,40 +133,6 @@ function container_flatten()
 }
 
 
-function container_commit()
-{
-	local container_name="$1"
-  local image_name="$2"
-  local tag="$3"
-  # TODO: Test whether the following variable assignment works:
-  local config="${@:4}"
-
-	echo "Committing container '$container_name' as image 'localhost/$image_name:$tag'... "
-	if ! container_exists "$container_name"; then
-		echo "Error: Container $container_name not found. Commit failed."
-		return 1;
-	fi
-
-  # Overwrite existing images without asking
-	if image_exists "localhost/$container_name"; then
-		$container_cli image rm "localhost/$container_name"
-	fi
-
-  # Apply
-  local config_args=""
-  for arg in $config; do
-    config_args="$config_args -c $arg"
-  done
-	export COMMIT_ID=$($container_cli commit --pause $config_args "$container_name")
-	echo "Commit id: $COMMIT_ID"
-
-  echo "Tagging as 'localhost/$image_name:$tag' ..."
-	$container_cli tag "$COMMIT_ID" "$image_name:$tag"
-
-  echo "Done."
-}
-
-
 function container_remove()
 {
 	local container="$1"
--- images.sh
+++ images.sh
@@ -19,7 +19,7 @@ function image_exists()
 	return 0;
 }
 
-function image_create_from_folder()
+function create_image_from_folder()
 {
   local path="$1"
   #local uri="$2"
@@ -30,4 +30,41 @@ function image_create_from_folder()
 
   # Add a tag to the new image
   #$container_cli tag "$COMMIT_ID" "$tag"
+}
+
+
+#
+# Commit a given (existing) container as a pushable Docker image
+#
+function create_image_from_container()
+{
+	local container_name="$1"
+  local image_name="$2"
+  local tag="$3"
+  # TODO: Test whether the following variable assignment works:
+  local config="${@:4}"
+
+	echo "Committing container '$container_name' as image 'localhost/$image_name:$tag'... "
+	if ! container_exists "$container_name"; then
+		echo "Error: Container $container_name not found. Commit failed."
+		return 1;
+	fi
+
+  # Overwrite existing images without asking
+	if image_exists "localhost/$container_name"; then
+		$container_cli image rm "localhost/$container_name"
+	fi
+
+  # Apply
+  local config_args=""
+  for arg in $config; do
+    config_args="$config_args -c $arg"
+  done
+	export COMMIT_ID=$($container_cli commit --pause $config_args "$container_name")
+	echo "Commit id: $COMMIT_ID"
+
+  echo "Tagging as 'localhost/$image_name:$tag' ..."
+	$container_cli tag "$COMMIT_ID" "$image_name:$tag"
+
+  echo "Done."
 }
