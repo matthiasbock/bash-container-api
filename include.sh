ff185380910e709c1e6e558eb0dfc76fc118ac96
Matthias Bock <mail@matthiasbock.net>
2023-01-27 16:45:19 +0100

    Making sure a container_cli is defined

--- include.sh
+++ include.sh
@@ -1,20 +1,47 @@
 
 #
-# Source this file in your bash script
-# in order to gain container mangement capabilities.
+# Include this file in your bash script
+# in order to make use of this library.
 #
 
-export container_cli="$(which podman)"
+#
+# This file may be included from a path-sensitive context,
+# therefore we must return to where we came from when we're done here.
+#
+oldpath="$(realpath "$(pwd)")"
 
-save_pwd=$(realpath $(pwd))
+# Move to this file's location
 cd $(realpath $(dirname "${BASH_SOURCE[0]}"))
 
-source networking.sh
-source images.sh
-source volumes.sh
-source containers.sh
-source user.sh
-source utils.sh
-source debian.sh
+# Include some helper functions operating locally
+. local.sh
+. web.sh
 
-cd "${save_pwd}"
+# Include functions for container manipulation
+. containers.sh
+. volumes.sh
+. images.sh
+. networking.sh
+. user.sh
+. utils.sh
+. debian.sh
+. expendables.sh
+
+# Finally, make sure a tool for container manipulation is defined
+if ! is_set container_cli; then
+  if is_program_available podman; then
+    container_cli="$(which podman)"
+    echo "Using $container_cli for container manipulation."
+  elif is_program_available docker; then
+    container_cli="$(which docker)"
+    echo "Using $container_cli for container manipulation."
+  else
+    # Should not be left empty, in case the container functions are called regardless.
+    container_cli="podman"
+    echo "Error: No program for container manipulation is available."
+  fi
+fi
+
+# Return to original location
+cd "$oldpath"
+unset oldpath
