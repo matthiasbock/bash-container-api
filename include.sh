2f305bf8db9b8eaafc221a58ebb436c457ad3124
Matthias Bock <mail@matthiasbock.net>
2023-02-03 19:49:57 +0100

    Adding comments to includes

--- include.sh
+++ include.sh
@@ -29,21 +29,43 @@ cd "$bash_container_library"
 assets_dir="$bash_container_library/assets"
 keys_dir="$assets_dir/keys"
 
-# Include some helper functions operating locally
+# Include functions for local tasks
 . src/utils/local.sh
+
+# Include functions to interact with the internet
 . src/utils/web.sh
+
+# Include functions for file checksum calculation and verification
 . src/utils/checksums.sh
 
-# Include functions for container manipulation
+# Include essential container control functions (create, start, stop etc.)
 . src/control.sh
+
+# Include functions for the management of container volumes
 . src/volumes.sh
+
+# Include functions for the management of container images
 . src/images.sh
+
+# Include functions to customize container networking
 . src/networking.sh
+
+# Include functions to conveniently manage users and groups within a container
 . src/users.sh
+
+# Include functions to conveniently manipulate files within a container
 . src/files.sh
+
+# Include functions for convenient package installation on Debian-based containers
 . src/debian/debian.sh
-. src/android/android.sh
+
+# Include more package installation helper functions
 . src/install.sh
+
+# Include functions for working with android images/containers
+. src/android/android.sh
+
+# Include functions for the removal of superfluous files from containers
 . src/expendables.sh
 
 #
