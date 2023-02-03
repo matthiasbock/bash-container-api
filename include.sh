2dd6f7fa36f7906302d2cc9ac76195a3e471e2ea
Matthias Bock <mail@matthiasbock.net>
2023-02-03 02:44:52 +0100

    Selecting container_cli

--- include.sh
+++ include.sh
@@ -31,8 +31,22 @@ cd "$bash_container_library"
 . src/install.sh
 . src/expendables.sh
 
-# Finally, make sure a tool for container manipulation is defined
-if ! is_set container_cli; then
+#
+# Make sure a tool for container manipulation is defined
+#
+if is_set CONTAINER_CLI; then
+  # Use cli defined by the caller
+  container_cli=CONTAINER_CLI
+fi
+if [ "$container_cli" != "" ]; then
+  if [ "$container_cli" == "stub" ]; then
+    # Use stub
+    container_cli="container_cli_stub"
+  elif ! is_program_available $container_cli; then
+    echo "Warning: Configured container CLI not found: \"$container_cli\""
+  fi
+else
+  # Guess the CLI to use
   if is_program_available podman; then
     container_cli="$(which podman)"
     echo "Using $container_cli for container manipulation."
@@ -41,8 +55,8 @@ if ! is_set container_cli; then
     echo "Using $container_cli for container manipulation."
   else
     # Should not be left empty, in case the container functions are called regardless.
-    container_cli="podman"
-    echo "Error: No program for container manipulation is available."
+    container_cli="container_cli_stub"
+    echo "Warning: No program for container manipulation is available."
   fi
 fi
 
