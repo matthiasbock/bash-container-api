782f5d4198e4bce8c7f10a572aee4aa9fc108160
Matthias Bock <mail@matthiasbock.net>
2023-02-03 19:58:05 +0100

    Moving definition of container_cli to separate file cli.sh

--- include.sh
+++ include.sh
@@ -38,6 +38,9 @@ keys_dir="$assets_dir/keys"
 # Include functions for file checksum calculation and verification
 . src/utils/checksums.sh
 
+# Define the program used for container manipualtion
+. src/cli.sh
+
 # Include essential container control functions (create, start, stop etc.)
 . src/control.sh
 
@@ -67,35 +70,6 @@ keys_dir="$assets_dir/keys"
 
 # Include functions for the removal of superfluous files from containers
 . src/expendables.sh
-
-#
-# Make sure a tool for container manipulation is defined
-#
-if is_set CONTAINER_CLI; then
-  # Use cli defined by the caller
-  container_cli="$CONTAINER_CLI"
-fi
-if [ "$container_cli" != "" ]; then
-  if [ "$container_cli" == "stub" ]; then
-    # Use stub
-    container_cli="container_cli_stub"
-  elif ! is_program_available $container_cli; then
-    echo "Warning: Configured container CLI not found: \"$container_cli\""
-  fi
-else
-  # Guess the CLI to use
-  if is_program_available podman; then
-    container_cli="$(which podman)"
-    echo "Using $container_cli for container manipulation."
-  elif is_program_available docker; then
-    container_cli="$(which docker)"
-    echo "Using $container_cli for container manipulation."
-  else
-    # Should not be left empty, in case the container functions are called regardless.
-    container_cli="container_cli_stub"
-    echo "Warning: No program for container manipulation is available."
-  fi
-fi
 
 # Return to original location
 cd "$old_workdir"
