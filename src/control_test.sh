b2f18bd229fb9dd6036781f316223c3d932d6856
Matthias Bock <mail@matthiasbock.net>
2023-02-07 12:19:25 +0100

    Elaborating tests for control.sh

--- src/control_test.sh
+++ src/control_test.sh
@@ -8,22 +8,46 @@ set -e
 testing_begins $0 $script_under_test
 
 # Import the functions under test
+. src/constants.sh
+. src/utils/local.sh
+. src/cli.sh
 . $script_under_test
 set +e
 
-echo "Testing get_container_names..."
-names=$(get_container_names)
+container_cli=podman
+container_name="bash-container-library-test"
+
+echo -n "Testing container creation... "
+container_create $container_name docker.io/debian:bullseye-slim amd64 2>/dev/null
 test_eval $?
 
-# container_exists
-# container_create
-# container_remove
+echo -n "Testing get_container_names()... "
+names=$(get_container_names 2>/dev/null)
+test_eval $?
 
-# container_start
-# container_is_running
-# container_stop
+echo -n "Testing container_exists()... "
+container_exists $container_name 2>/dev/null
+test_eval $?
 
-# container_exec
+echo -n "Attempting to start container... "
+container_start $container_name 2>/dev/null
+test_eval $?
+
+echo -n "Testing container_is_running()... "
+container_is_running $container_name 2>/dev/null
+test_eval $?
+
+echo -n "Executing a command in the container... "
+container_exec $container_name echo test123 2>/dev/null
+test_eval $?
+
+echo -n "Attempting to stop container... "
+container_stop $container_name 2>/dev/null
+test_eval $?
+
+echo -n "Testing container removal... "
+container_remove $container_name 2>/dev/null
+test_eval $?
 
 # Finished testing
 testing_ends $0 $script_under_test
