05e2f29d53e3ff2833b4a530536ea45c6e84a2b2
Matthias Bock <mail@matthiasbock.net>
2023-02-07 11:15:18 +0100

    Redirecting output to stderr

--- src/cli.sh
+++ src/cli.sh
@@ -15,18 +15,18 @@ fi
 if is_set container_cli; then
   # Check whether the configured program is available
   if ! is_program_available $container_cli; then
-    echo "Warning: Configured container CLI not found: \"$container_cli\""
+    echo "Warning: Configured container CLI not found: \"$container_cli\"" >&2
   fi
 else
   # No program is defined, choose one that is available
   if is_program_available podman; then
     container_cli="$(which podman)"
-    echo "Using $container_cli for container manipulation."
+    echo "Using $container_cli for container manipulation." >&2
   elif is_program_available docker; then
     container_cli="$(which docker)"
-    echo "Using $container_cli for container manipulation."
+    echo "Using $container_cli for container manipulation." >&2
   else
-    echo "Error: No program for container manipulation is available."
+    echo "Error: No program for container manipulation is available." >&2
   fi
 fi
 if [ "$container_cli" == "" ]; then
